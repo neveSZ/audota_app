0cae7f1574ef9b55045db90550ee66f267b57905
Victor Neves Silva <victor@nevesz.me>
2021-06-07 23:02:45 -0300

    adicionado funcao para adicionar e editar animais

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -13,7 +13,44 @@ class AnimalsController < ApplicationController
     @favoritos_atuais = favoritos_atuais
   end
 
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    @animal = Animal.new(animal_params)
+    @animal.status = 'disponivel'
+    if @animal.save
+      redirect_to @animal, notice: 'Animal cadastrado com sucesso'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @animal = Animal.find(params[:id])
+  end
+
+  def update
+    @animal = Animal.find(params[:id])
+    if @animal.update(animal_params)
+      redirect_to @animal
+    else
+      render :edit
+    end
+  end
+
   def filter_params(params)
     params.slice(:cor, :tipo_pelo, :tamanho_pelo, :porte, :idade_min, :idade_max, :search)
+  end
+
+  private def animal_params
+    animal_params = params.require(:animal).permit(:nome, :peso, :descricao, :nascimento, :cor, :tipo_pelo, :tamanho_pelo,
+                                                   :porte)
+    animal_params[:cor] = animal_params[:cor].to_i
+    animal_params[:tipo_pelo] = animal_params[:tipo_pelo].to_i
+    animal_params[:tamanho_pelo] = animal_params[:tamanho_pelo].to_i
+    animal_params[:porte] = animal_params[:porte].to_i
+    animal_params
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
   devise_for :admins
   root to: 'home#index'
-  resources :animals, only: %i[index show]
-  resources :orders, only: %i[create new]
+  resources :animals, only: %i[index show create new edit update]
+  resources :orders, only: %i[create new index show]
   resources :favoritos, only: %i[create index]
   delete 'favoritos(.:format)', to: 'favoritos#destroy'
-  scope :admin, as: 'admin' do
-    get '/', to: 'admin#index'
-    resources :animals, only: %i[create new edit show index]
-    resources :orders, only: %i[index show]
-    post '/orders/:id', to: 'orders#concluir', as: 'concluir_order'
-    delete '/orders/:id', to: 'orders#cancelar', as: 'cancelar_order'
-  end
+  get '/admin', to: 'admin#index'
+  post '/orders/:id', to: 'orders#concluir', as: 'concluir_order'
+  delete '/orders/:id', to: 'orders#cancelar', as: 'cancelar_order'
 end
